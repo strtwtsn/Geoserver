7d56844fc50c6bbf562868e0a3365d8aa92adc72
strtwtsn <stuart.watson@unep-wcmc.org>
2014-03-14 13:37:03 +0000

    Update default.rb

--- cookbooks/geoserver/recipes/default.rb
+++ cookbooks/geoserver/recipes/default.rb
@@ -1,19 +1,13 @@
 package "unzip"
 
-#remote_file "Download Tomcat" do
-#path "/home/ubuntu/downloads/geoserver-2.4.5-war.zip"
-#source "http://sourceforge.net/projects/geoserver/files/GeoServer/2.4.5/geoserver-2.4.5-war.zip"
-#end
+remote_file "Download Tomcat" do
+path "/home/ubuntu/downloads/geoserver-2.4.5-war.zip"
+source "http://sourceforge.net/projects/geoserver/files/GeoServer/2.4.5/geoserver-2.4.5-war.zip"
+end
 
 service "tomcat7" do
   service_name "tomcat7"
   supports :restart => true, :status => true, :reload => true
-end
-
-
-template "/var/lib/tomcat7/webapps/geoserver/WEB-INF/web.xml" do
-source "web.xml.erb"
-notifies :restart, resources(:service => "tomcat7"), :immediately
 end
 
 bash "Extract Geoserver" do
@@ -27,6 +21,11 @@ chown -R tomcat7:tomcat7 /geoserver_data/
 echo -e \"% export GEOSERVER_DATA_DIR=/geoserver_data\" | sudo tee -a /root/.bashrc
 echo -e \"CATALINA_OPTS=\"-DGEOSERVER_DATA_DIR=/geoserver_data\"\" | sudo tee -a /usr/share/tomcat7/bin/setclasspath.sh
 EOH
+end
+
+template "/var/lib/tomcat7/webapps/geoserver/WEB-INF/web.xml" do
+source "web.xml.erb"
+notifies :restart, resources(:service => "tomcat7"), :immediately
 end
 
 
